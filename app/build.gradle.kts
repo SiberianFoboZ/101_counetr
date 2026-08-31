91d978e9b738424e6f0c14e1ccf6ed054433af87
Владимир Куприянов <Vladimir.Kupriyanov@centre-it.com>
2026-09-01 01:02:32 +0700

    chore: bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.counter.game"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
